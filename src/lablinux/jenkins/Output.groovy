e8fd22ef00bfd74ba1ea75f552a196e9499edbbf
lablinux80 <junius.widodo@gmail.com>
2023-05-12 20:13:26 +0700

    add steps in groovy function

--- src/lablinux/jenkins/Output.groovy
+++ src/lablinux/jenkins/Output.groovy
@@ -2,7 +2,7 @@ package lablinux.jenkins
 
 class Output {
 	
-	static def hello(String name) {
-		println("Hello ${name}")
+	static def hello(steps, String name) {
+		steps.echo("Hello ${name}")
 	}
 }
